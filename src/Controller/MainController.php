38ec2a0fd302e857dc2d203c2ce101d31a04cca8
Gaétan GURU <14243@ecam.be>
2018-03-09 12:00:24 +0100

    editNote and deleteNote added but none tested

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -15,10 +15,9 @@ class MainController extends Controller
     * @Route("/note/main", name="noteMain")
     *
     */
+    //TODO : change main to addNote
     public function main(Request $request)
     {
-        // createFormBuilder is a shortcut to get the "form factory"
-        // and then call "createBuilder()" on it
         $defaults = array(
             'dueDate' => new \DateTime('tomorrow'),
         );
@@ -49,17 +48,41 @@ class MainController extends Controller
     {
         $repository = $this->getDoctrine()->getRepository(Note::class);
         $notes = $repository->findAll();
-        // $arrayNotes = array();
-        // $informationsNotes = array();
-        // foreach($notes as $note){
-        //   // array_push($informationsNotes, $note->getId());
-        //   array_push($informationsNotes, $note->getCategory());
-        //   array_push($informationsNotes, $note->getTitle());
-        //   array_push($informationsNotes, $note->getContent());
-        //   array_push($arrayNotes, $informationsNotes);
-        //   $informationsNotes = array();
-        // }
+
         return $this->render('note/homePage.html.twig', array('notes' => $notes, ));
+    }
+    /**
+    * @Route("/note/edit", name="editNote")
+    *
+    */
+    public function editNote(Request $request){
+
+        $note = new Note;
+        //TODO : have to change the names of the button in the form
+        $form = $this->createForm(AddNote::class, $note);
+        $form->handleRequest($request);
+        //TODO : have to change the name of the button in the following if
+        if ($form->isSubmitted() && $form->isValid() && 'save' === $form->getClickedButton()->getName()) {
+            $note = $form->getData();
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($note);
+            $entityManager->flush();
+            return $this->redirectToRoute('home');
+        }
+        return $this->render('note/addNote.html.twig', array(
+            'form' => $form->createView(),
+        ));
+    }
+    /**
+    * @Route("/note/delete", name="deleteNote")
+    *
+    */
+    public function deleteNote(Note $note){
+        $entityManager = $this->getDoctrine()->getManager();
+        $entityManager->remove($note);
+        $entityManager->flush();
+        return redirectToRoute("home");
+        }
     }
 }
 ?>
