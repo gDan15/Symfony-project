be0c09331937c2f7b48c9c62bedf0660b0d3256c
Gaétan GURU <14243@ecam.be>
2018-03-16 12:22:33 +0100

    Still implementing api

--- src/Controller/ControllerApi.php
+++ src/Controller/ControllerApi.php
@@ -6,20 +6,42 @@ use App\Entity\Notes;
 use App\Entity\Category;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Response;
-
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-
+use Symfony\Component\HttpFoundation\Request;
 
 class ControllerApi extends Controller
 {
     /**
-     * @Route("/controller/api", name="controllerApi")
-     * 
+     * @Route("/note/api/get", name="noteApiGet")
+     * @Method({"GET"})
      */
     public function index()
     {
         $entityManager = $this->getDoctrine(Note::class)->getRepository();
         $notes = $entityManager->findAll();
-
+        $data = $this->get('jms_serializer')->serialize($notes, 'json');
+        $response = new Response($data);
+        $response->headers->set('Content-Type', 'application/json');
+        return $response;
+    }
+    /**
+      * @Route("/note/api/post", name="noteApiPost")
+      * @Method({"POST"})
+      * @param Request $request
+      * @return \Symfony\Component\HttpFoundation\RedirectResponse|Response
+      */
+    public function newNote(Request $request)
+    {
+       $entityManager = $this->getDoctrine()->getManager();
+       $content = $request->getContent();
+       if(empty($content))
+       {
+         return new JsonResponse(array('status'=>'EMPTY','message'=>'The body of this request is empty.'));
+       }
+       $note = $this->get('jms_serializer')->deserialize($content, Note::class, 'json');
+       $noteManager->persist($note);
+       $noteManager->flush();
+       $response = new JsonResponse();
     }
 }
