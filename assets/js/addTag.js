e72a2db7e4b8504feb903ffabbc019ceec9fb1b2
Gaétan Guru <gaetan.guru@gmail.com>
2018-04-23 20:20:03 +0200

    tag still doesn't work

---
+++ assets/js/addTag.js
@@ -0,0 +1,38 @@
+// assets/js/addTag.js
+
+// loads the jquery package from node_modules
+var $ = require('jquery');
+
+// import the function from greet.js (the .js extension is optional)
+// ./ (or ../) means to look for a local file
+// var greet = require('./greet');
+//
+// $(document).ready(function() {
+//     $('body').prepend('<h1>'+greet('john')+'</h1>');
+// });
+function wrapText(elementID, openTag, closeTag) {
+
+  var textArea = $('#' + elementID);
+  alert(textArea);
+  var len = textArea.val().length;
+  var start = textArea[0].selectionStart;
+  var end = textArea[0].selectionEnd;
+  var selectedText = textArea.val().substring(start, end);
+  alert(selectedText);
+  var replacement = openTag + selectedText + closeTag;
+  alert(replacement);
+  textArea.val(textArea.val().substring(0, start) + replacement + textArea.val().substring(end, len));
+}
+$('#addTag').click(function() {
+  // alert("Yeah!");
+  wrapText("notepadbundle_note_content", "<tag>", "</tag>");
+});
+// window.onload = function() {
+//     if (window.jQuery) {
+//         // jQuery is loaded
+//         alert("Yeah!");
+//     } else {
+//         // jQuery is not loaded
+//         alert("Doesn't Work");
+//     }
+// }
